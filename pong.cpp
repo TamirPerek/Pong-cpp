d8a8978cd0fa2e7fc3406f47225ac521ab7f0829
Nils Brödner <nils.broedner@cgm.com>
2023-06-12 12:19:28 +0200

    Made it work under windows

--- pong.cpp
+++ pong.cpp
@@ -16,16 +16,16 @@ class UIElement
 public:
 	SDL_Rect mRect;
 
-	explicit UIElement(const SDL_Rect &xRect) : mRect{xRect} {}
-	explicit UIElement(SDL_Rect &&xRect) : mRect{std::move(xRect)} {}
+	explicit UIElement(const SDL_Rect& xRect) : mRect{ xRect } {}
+	explicit UIElement(SDL_Rect&& xRect) : mRect{ std::move(xRect) } {}
 
-	~UIElement() = default;
-	virtual void update(const WindowSize &xWindowSize) noexcept = 0;
-	operator SDL_Rect const *() const
+	virtual ~UIElement() = default;
+	virtual void update(const WindowSize& xWindowSize) noexcept = 0;
+	explicit operator SDL_Rect const* () const
 	{
 		return &mRect;
 	}
-	operator SDL_Rect *()
+	explicit operator SDL_Rect* ()
 	{
 		return &mRect;
 	}
@@ -36,13 +36,15 @@ class Player : public UIElement
 public:
 	WindowSize mWindowSize;
 
-	explicit Player(const WindowSize &xWindowSize) : mWindowSize{xWindowSize}, UIElement{SDL_Rect{0, 0, xWindowSize.w / 40, xWindowSize.h / 4}}
+	explicit Player(const WindowSize& xWindowSize) 
+		: UIElement{ SDL_Rect{0, 0, xWindowSize.w / 40, xWindowSize.h / 4} },
+		mWindowSize{ xWindowSize }
 	{
 	}
 
-	~Player() = default;
+	~Player() final = default;
 
-	void update(const WindowSize &xWindowSize) noexcept final
+	void update(const WindowSize& xWindowSize) noexcept final
 	{
 		if (xWindowSize == mWindowSize)
 			return;
@@ -60,29 +62,29 @@ class Ball : public UIElement
 {
 public:
 	WindowSize mWindowSize;
-	double mXSpeed{2.0};
-	double mYSpeed{1.0};
+	double mXSpeed{ 2.0 };
+	double mYSpeed{ 1.0 };
 	constexpr static double mForce = 1.0005;
 
-	explicit Ball(const WindowSize &xWindowSize)
-		: mWindowSize{xWindowSize},
-		  UIElement{SDL_Rect{0, 0, static_cast<int>((xWindowSize.h / 50) * xWindowSize.hRatio), static_cast<int>((xWindowSize.h / 50) * xWindowSize.hRatio)}}
+	explicit Ball(const WindowSize& xWindowSize)
+		: UIElement{ SDL_Rect{0, 0, static_cast<int>((xWindowSize.h / 50) * xWindowSize.hRatio), static_cast<int>((xWindowSize.h / 50) * xWindowSize.hRatio)} },
+		mWindowSize{ xWindowSize }
 	{
 		Resett();
 	}
-	~Ball() = default;
+	~Ball() final = default;
 
-	void update(const WindowSize &xWindowSize) noexcept final
+	void update(const WindowSize& xWindowSize) noexcept final
 	{
 		assert(false);
 	}
 
-	void update(const WindowSize &xWindowSize, const Player &xPlayerOne, const Player &xPlayerTwo) noexcept
+	void update(const WindowSize& xWindowSize, const Player& xPlayerOne, const Player& xPlayerTwo) noexcept
 	{
-		if (mXSpeed > 0 && SDL_HasIntersection(*this, xPlayerOne) == SDL_TRUE)
+		if (mXSpeed > 0 && SDL_HasIntersection(static_cast<const SDL_Rect*>(*this), static_cast<const SDL_Rect*>(xPlayerOne)) == SDL_TRUE)
 			mXSpeed *= -1.0;
 
-		if (mXSpeed < 0 && SDL_HasIntersection(*this, xPlayerTwo) == SDL_TRUE)
+		if (mXSpeed < 0 && SDL_HasIntersection(static_cast<const SDL_Rect*>(*this), static_cast<const SDL_Rect*>(xPlayerTwo)) == SDL_TRUE)
 			mXSpeed *= -1.0;
 
 		if (mRect.y < 0 || mRect.y + mRect.h > mWindowSize.h)
@@ -106,7 +108,7 @@ public:
 
 	void Resett() noexcept
 	{
-		static auto tGen{std::bind(std::uniform_int_distribution<>(0, 1), std::default_random_engine())};
+		static auto tGen{ std::bind_front(std::uniform_int_distribution<>(0, 1), std::default_random_engine()) };
 
 		mRect.x = mWindowSize.w / 2;
 		mRect.y = mWindowSize.h / 2;
@@ -119,49 +121,46 @@ class Points : public UIElement
 {
 public:
 	WindowSize mWindowSize;
-	// TTF_Font *Sans{TTF_OpenFont("Sans.ttf", 24)};
-	// SDL_Color White{255, 255, 255};
-	unique_font_t mFont{TTF_OpenFont("assets/DS-DIGI.TTF", 24)};
-	unsigned int mValueOne{0U};
-	unsigned int mValueTwo{0U};
-	// unique_surface_t mMessage;
+	unique_font_t mFont{ TTF_OpenFont("assets/DS-DIGI.TTF", 24) };
+	unsigned int mValueOne{ 0U };
+	unsigned int mValueTwo{ 0U };
 
-	explicit Points(const WindowSize &xWindowSize)
-		: mWindowSize{xWindowSize},
-		  UIElement{SDL_Rect{0, 0, 500, 200}}
+	explicit Points(const WindowSize& xWindowSize)
+		: UIElement{ SDL_Rect{0, 0, 500, 200} },
+		mWindowSize{ xWindowSize }
 	{
 		if (!mFont)
 		{
-			constexpr static std::string_view errormsg{"Error: unable to load font\n"};
+			constexpr static std::string_view errormsg{ "Error: unable to load font\n" };
 			std::cerr << errormsg;
 			throw std::runtime_error(errormsg.data());
 		}
 	}
 
-	~Points() = default;
+	~Points() final = default;
 
-	void update(const WindowSize &xWindowSize) noexcept final
+	void update(const WindowSize& xWindowSize) noexcept final
 	{
 		assert(false);
 	}
 
-	void update(const WindowSize &xWindowSize, unique_renderer_t &xRenderer) noexcept
+	void update(const WindowSize& xWindowSize, const unique_renderer_t& xRenderer) noexcept
 	{
 		auto tScore = fmt::format("{} : {}", mValueOne, mValueTwo);
-		unique_surface_t tMessageSurface{TTF_RenderText_Solid(mFont.get(), tScore.c_str(), SDL_Color{255, 255, 255})};
+		unique_surface_t tMessageSurface{ TTF_RenderText_Solid(mFont.get(), tScore.c_str(), SDL_Color{255, 255, 255}) };
 		if (!tMessageSurface)
 		{
 			std::cerr << "Unable to render text\n";
 		}
 
-		unique_texture_t Message{SDL_CreateTextureFromSurface(xRenderer.get(), tMessageSurface.get())};
+		unique_texture_t Message{ SDL_CreateTextureFromSurface(xRenderer.get(), tMessageSurface.get()) };
 		if (!Message)
 		{
 			std::cerr << "Unable to create texture\n";
 			return;
 		}
 
-		if (SDL_RenderCopy(xRenderer.get(), Message.get(), nullptr, *this) != 0)
+		if (SDL_RenderCopy(xRenderer.get(), Message.get(), nullptr, static_cast<const SDL_Rect*>(*this)) != 0)
 			std::cerr << "Unable to show points\n";
 
 		if (mWindowSize == xWindowSize)
@@ -176,39 +175,37 @@ public:
 	}
 };
 
-int main()
+int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		return EXIT_FAILURE;
 	if (TTF_Init() != 0)
 		return EXIT_FAILURE;
 
-	unique_window_t window{SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1028, 720, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI)};
-	unique_renderer_t tRenderer{SDL_CreateRenderer(window.get(), -1, 0U)};
-	int x = 580;
-	int y = 160;
+	unique_window_t window{ SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1028, 720, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI) };
+	unique_renderer_t tRenderer{ SDL_CreateRenderer(window.get(), -1, 0U) };
 
-	WindowSize tWindowSize{window};
+	WindowSize tWindowSize{ window };
 
-	Player tPlayerOne{tWindowSize};
+	Player tPlayerOne{ tWindowSize };
 	tPlayerOne.mRect.x = tWindowSize.w - (tPlayerOne.mRect.w * 2);
 	tPlayerOne.mRect.y = (tWindowSize.h / 2) - (tPlayerOne.mRect.h / 2);
 
-	Player tPlayerTwo{tWindowSize};
+	Player tPlayerTwo{ tWindowSize };
 	tPlayerTwo.mRect.x = tPlayerTwo.mRect.w;
 	tPlayerTwo.mRect.y = (tWindowSize.h / 2) - (tPlayerTwo.mRect.h / 2);
 
-	Ball tBall{tWindowSize};
+	Ball tBall{ tWindowSize };
 
-	Points tPoints{tWindowSize};
+	Points tPoints{ tWindowSize };
 	tPoints.mRect.x = (tWindowSize.w / 2) - (tPoints.mRect.w / 2);
 
 	bool isquit = false;
 	SDL_Event event;
 
-	bool run{true};
+	bool run{ true };
 
-	std::map<int, bool> tKeyPressed{{SDLK_UP, false}, {SDLK_DOWN, false}, {SDLK_w, false}, {SDLK_s, false}, {SDLK_ESCAPE, false}};
+	std::map<int, bool> tKeyPressed{ {SDLK_UP, false}, {SDLK_DOWN, false}, {SDLK_w, false}, {SDLK_s, false}, {SDLK_ESCAPE, false} };
 	while (!isquit)
 	{
 		SDL_Delay(10);
@@ -232,13 +229,13 @@ int main()
 		}
 
 		if (tKeyPressed.at(SDLK_UP) && tPlayerOne.mRect.y > 0)
-			tPlayerOne.mRect.y -= (5 * tWindowSize.hRatio);
+			tPlayerOne.mRect.y -= static_cast<int>(5 * tWindowSize.hRatio);
 		if (tKeyPressed.at(SDLK_DOWN) && tPlayerOne.mRect.y + tPlayerOne.mRect.h < tWindowSize.h)
-			tPlayerOne.mRect.y += (5 * tWindowSize.hRatio);
+			tPlayerOne.mRect.y += static_cast<int>(5 * tWindowSize.hRatio);
 		if (tKeyPressed.at(SDLK_w) && tPlayerTwo.mRect.y > 0)
-			tPlayerTwo.mRect.y -= (5 * tWindowSize.hRatio);
+			tPlayerTwo.mRect.y -= static_cast<int>(5 * tWindowSize.hRatio);
 		if (tKeyPressed.at(SDLK_s) && tPlayerTwo.mRect.y + tPlayerTwo.mRect.h < tWindowSize.h)
-			tPlayerTwo.mRect.y += (5 * tWindowSize.hRatio);
+			tPlayerTwo.mRect.y += static_cast<int>(5 * tWindowSize.hRatio);
 		if (tKeyPressed.at(SDLK_ESCAPE))
 		{
 			isquit = true;
@@ -248,7 +245,7 @@ int main()
 		if (!run)
 			continue;
 
-		tWindowSize = WindowSize{window};
+		tWindowSize = WindowSize{ window };
 		tPlayerOne.update(tWindowSize);
 		tPlayerTwo.update(tWindowSize);
 		tBall.update(tWindowSize, tPlayerOne, tPlayerTwo);
@@ -259,13 +256,13 @@ int main()
 		tPoints.update(tWindowSize, tRenderer);
 
 		SDL_SetRenderDrawColor(tRenderer.get(), 255, 255, 255, 255);
-		SDL_RenderFillRect(tRenderer.get(), tBall);
+		SDL_RenderFillRect(tRenderer.get(), static_cast<const SDL_Rect*>(tBall));
 
 		SDL_SetRenderDrawColor(tRenderer.get(), 255, 255, 255, 255);
-		SDL_RenderFillRect(tRenderer.get(), tPlayerOne);
+		SDL_RenderFillRect(tRenderer.get(), static_cast<const SDL_Rect*>(tPlayerOne));
 
 		SDL_SetRenderDrawColor(tRenderer.get(), 255, 255, 255, 255);
-		SDL_RenderFillRect(tRenderer.get(), tPlayerTwo);
+		SDL_RenderFillRect(tRenderer.get(), static_cast<const SDL_Rect*>(tPlayerTwo));
 
 		SDL_RenderPresent(tRenderer.get());
 
@@ -274,7 +271,7 @@ int main()
 		{
 			tBall.Resett();
 			++tPoints.mValueTwo;
-			if(tPoints.mValueTwo == 9)
+			if (tPoints.mValueTwo == 9)
 				run = false;
 		}
 
